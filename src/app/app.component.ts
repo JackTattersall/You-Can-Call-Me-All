64f9fa27c4190a7e9f3cfb2e374bed7a1d4d78c1
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-10-30 20:31:43 +0000

    removes green line adds nav hide and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  readerView = false;
+  navShow = false;
+
+  constructor(private router: Router) {}
+
+  onNavShow() {
+    this.navShow = !this.navShow;
+  }
+
+  onReaderView() {
+    this.readerView = !this.readerView;
+  }
+
 }
